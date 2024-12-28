81a1e03d68d36fd3acb22465a8c896520d128bb3
Pranay Haldiya <pranayhaldiya8127@gmail.com>
2024-12-28 13:47:09 +0530

    Update firebase.config.js

--- client/src/firebase.config.js
+++ client/src/firebase.config.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDUk5A2b4P-qb2ieLmR0ckKXle1I8BQY4g",
-  authDomain: "bot-talk-3d11c.firebaseapp.com",
-  projectId: "bot-talk-3d11c",
-  storageBucket: "bot-talk-3d11c.firebasestorage.app",
-  messagingSenderId: "1054292915455",
-  appId: "1:1054292915455:web:8c4f19c1f023c5d0ba48ca",
-  measurementId: "G-7E4N33FDY9"
+apiKey: process.env.FIREBASE_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
